a26b9f52561c720102ee83d84090f3bac67ffc73
Isadora Miranda <isadora.miranda01087997@sesisenaipr.org.br>
2026-02-19 10:04:21 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 15 # variável inteira
+peso = 55.4 # variável float 
+nome = "Isadora Miranda" # variável string 
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
