7d35094ca97b57900074f0bbd9012510761c518d
Aluno <isadora.miranda01087997@sesisenaipr.org>
2026-02-20 09:45:26 -0300

    aula input

---
+++ matematica.py
@@ -0,0 +1,13 @@
+valor1 = int(input("digite o primeiro número"))
+valor2 = int(input("digite o primeiro número"))
+
+
+soma = valor1 + valor2 
+subtracao = valor1 - valor2 
+divisao = valor1/valor2 
+multiplicacao = valor1 * valor2 
+
+print("a soma dos valores é:", soma)
+print("a subtração dos valores é:", subtracao)
+print("a divisão dos valores é:", divisao)
+print("a multiplicação dos valores é:", multiplicacao)
