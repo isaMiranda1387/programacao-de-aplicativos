35e57317839d6b00a522d453ef86387c99ec3344
Isadora <isadora.miranda01087997@sesisenaipr.org.br>
2026-03-11 11:59:25 -0300

    11/03

--- logistica.py
+++ logistica.py
@@ -4,5 +4,7 @@ peso = float(input("Qual o peso do pacote? "))
 
 if peso < 5 and (codigo % 10 == 0):
     print(f"Pacote {codigo}: ENTREGA EXPRESSA")
-elif peso > 50 :
+elif peso > 50:
     print(f"Pacote {codigo}: CARGA PESADA")    
+else:
+    print("Informações inválidas.")    
